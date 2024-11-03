119f7262fedc17062f067128bcf46e237f6384ef
wuyuheng111 <wyhdzy@outlook.com>
2024-11-03 19:39:46 +0800

    Update EventService.ts

--- vite-project/src/services/EventService.ts
+++ vite-project/src/services/EventService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/2144768106/lab3/',
+  baseURL: 'https://my-json-server.typicode.com/wuyuheng111/lab3/',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
